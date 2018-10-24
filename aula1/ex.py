1135d800ab2cb00bfcd2cc796423f83d9fe5b992
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-10-23 23:10:39 -0200

    aula1

---
+++ aula1/ex.py
@@ -0,0 +1,13 @@
+idade = int(input('qual é a sua idade?'))
+if idade>=18:
+    print('você pode entrar.')
+else:
+    acompanhamento=input('você esta acompanhado de uma pessoa maior deidade?')
+    
+    if acompanhamento == 's': 
+        print('você pode entrar')
+    else:
+        print('você não pode entrar')
+
+
+        
---
+++ aula1/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+mensagem = 'Seja bem vindo'
+nome = input("Digite seu nome: ")
+print(mensagem,nome)
+
+
+x=10
+type (x)
